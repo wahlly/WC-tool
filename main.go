df97c5af0eeeb17a6d4e3e853b8cf03c00c3dedb
wahllyWest <olaifaolawaleh@gmail.com>
2024-07-10 23:01:32 +0100

    get file size in bytes

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+
+func main () {
+	reader := bufio.NewReader(os.Stdin)
+	val, _ := reader.ReadString('\n')
+	val = strings.TrimSpace(val)
+	opts := strings.Split(val, " ")
+
+	if len(opts) != 3 {
+		fmt.Println("Invalid command!")
+		return
+	}
+
+	if opts[0] != "ccwc" {
+		fmt.Println("Invalid command!")
+		return
+	}
+
+	if opts[1] == "-c" {
+		getFileByteSize(opts[2])
+		return
+	}
+}
